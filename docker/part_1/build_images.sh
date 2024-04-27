ba198fc540a93f1bffdd709cb3d1fe4285a30e12
moAmza <mohamad.zangooei@outlook.com>
2024-04-27 11:51:20 +0330

    fix: modify docker questions

--- docker/part_1/build_images.sh
+++ docker/part_1/build_images.sh
@@ -1,6 +1,8 @@
-docker build -f ./images/Dockerfile.test -t rahnema:test ./images
-docker build -f ./images/Dockerfile.prod -t rahnema:prod -t rahnema:latest ./images 
+rahnema=registry.hamdocker.ir/mohamadzangooei/rahnema
 
-# docker push ...:test 
-# docker push ...:prod 
-# docker push ...:latest 
+docker build -f ./images/Dockerfile.test -t $rahnema:test ./images
+docker build -f ./images/Dockerfile.prod -t $rahnema:prod -t $rahnema:latest ./images 
+
+docker push $rahnema:test 
+docker push $rahnema:prod 
+docker push $rahnema:latest 
