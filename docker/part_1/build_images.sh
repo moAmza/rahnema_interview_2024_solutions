398d61460dcfc8ed38364eb4950ebda638f6f70e
moAmza <mohamad.zangooei@outlook.com>
2024-04-27 16:46:40 +0330

    fix: remove docker solutions from git

--- docker/part_1/build_images.sh
+++
@@ -1,8 +0,0 @@
-rahnema=registry.hamdocker.ir/mohamadzangooei/rahnema
-
-docker build -f ./images/Dockerfile.test -t $rahnema:test ./images
-docker build -f ./images/Dockerfile.prod -t $rahnema:prod -t $rahnema:latest ./images 
-
-docker push $rahnema:test 
-docker push $rahnema:prod 
-docker push $rahnema:latest 
